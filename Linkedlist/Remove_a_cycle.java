cfba5c66b1df9fa7eff9e03967565983b7afeb51
Rohan Adkine <rohanadkine@gmail.com>
2024-08-04 18:30:35 +0530

    Remove a Cycle in a LL

---
+++ Linkedlist/Remove_a_cycle.java
@@ -0,0 +1,77 @@
+//Applicable only to head to not head cycle
+
+public class Remove_a_cycle {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public static void removeCycle() {
+        // Detect cycle
+        Node slow = head;
+        Node fast = head;
+        boolean cycle = false;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (fast == slow) {
+                cycle = true;
+                break;
+            }
+        }
+        if (!cycle) {
+            return;
+        }
+
+        // Find meeting point
+        slow = head;
+        Node prev = null; // last node
+        while (slow != fast) {
+            prev = fast;
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        // Remove cycle -> last.next = null
+        prev.next = null;
+    }
+
+    public static void print() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "-->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String args[]) {
+        head = new Node(1);
+        Node second = new Node(2);
+        Node third = new Node(3);
+        Node fourth = new Node(4);
+        Node fifth = new Node(5);
+
+        head.next = second;
+        second.next = third;
+        third.next = fourth;
+        fourth.next = fifth;
+        fifth.next = third; // Creating a cycle here
+        removeCycle();
+        print();
+    }
+}
+
+    
+
+
+

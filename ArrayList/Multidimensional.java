ac906ba3904b86ba7e1a8146d530a66c86486338
Rohan Adkine <rohanadkine@gmail.com>
2024-08-15 09:53:50 +0530

    Multidimensional Arraylist

--- ArrayList/Multidimensional.java
+++ ArrayList/Multidimensional.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-public class Multidimenional {
+public class multidimensional {
     public static void main(String args[]) {
         ArrayList<ArrayList<Integer>> mainList = new ArrayList<>();
         ArrayList<Integer> list1 = new ArrayList<>();
